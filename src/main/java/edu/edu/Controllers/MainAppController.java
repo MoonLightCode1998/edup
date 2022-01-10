481cc2181a05e3c246da1eeb626ae209b33175db
MoonLightCode <jbondaruk02@gmail.com>
2022-01-10 22:03:51 +0100

    Added GetRequest for registration page.

--- src/main/java/edu/edu/Controllers/MainAppController.java
+++ src/main/java/edu/edu/Controllers/MainAppController.java
@@ -3,12 +3,17 @@ package edu.edu.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequestMapping
 public class MainAppController {
     @GetMapping("/")
     public String homePage(){
-        return "homepage";
+        return "homePage";
+    }
+    @GetMapping("rejestracja")
+    public String rejestracja(){
+        return "registrationForm";
     }
 }
